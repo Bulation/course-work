98e0409d2d3e498be025a3e80404d6ef3c8b59b9
Bulation <insanbulat@gmail.com>
2023-01-27 23:11:57 +0300

    feat: add navigation component

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,40 @@
+import { NavLink } from 'react-router-dom';
+import { LinkType } from '../../types/LinkType';
+import Icon from '../Icon/Icon';
+
+interface INavigationProps {
+  links: LinkType[];
+  styles: {
+    navigationLink: string;
+    navigationList: string;
+    navigationItem: string;
+    navigationSvg?: string;
+    active?: string;
+  };
+}
+
+export default function Navigation(props: INavigationProps) {
+  const { links, styles } = props;
+  const setActive:
+    | ((props: { isActive: boolean; isPending: boolean }) => string | undefined)
+    | undefined = ({ isActive }) =>
+    isActive ? `${styles.active} ${styles.navigationLink}` : styles.navigationLink;
+  return (
+    <nav>
+      <ul className={styles.navigationList}>
+        {links.map((link) => {
+          return (
+            <li className={styles.navigationItem} key={link.content}>
+              <NavLink className={setActive} to={link.address}>
+                {link.svgId && styles.navigationSvg ? (
+                  <Icon id={link.svgId} className={styles.navigationSvg}></Icon>
+                ) : null}
+                {link.content}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
