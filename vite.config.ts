ad61f232db0b2ffbbfd05a7366faf51f6195f919
Bulat <insanbulat@gmail.com>
2023-02-10 23:58:58 +0300

    feat: add base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
+  base: '/course-work/',
   plugins: [react(), legacy()],
 });
