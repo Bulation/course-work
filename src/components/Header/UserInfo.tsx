7932ed3da7b246b65cac9581a300178374e45bee
bulation <insanbulat@gmail.com>
2023-02-10 23:01:16 +0300

    refactor: refactor code and add logic for logout

--- src/components/Header/UserInfo.tsx
+++ src/components/Header/UserInfo.tsx
@@ -1,25 +1,30 @@
-import { useEffect, useState } from 'react';
-import Icon from '../Icon/Icon';
+import { useRef } from 'react';
+import useClickOutside from '../../hooks/useClickOutside';
 import styles from './header.module.css';
 
 export default function UserInfo() {
-  const [username, setUsername] = useState('');
-  useEffect(() => {
-    const storageValue = localStorage.getItem('username');
-    if (storageValue) {
-      setUsername(storageValue);
-    }
-  });
-  // get info about username: username
-  // more-info span - add arrow
-  // get info about img
+  const dropDownNode = useRef<HTMLDivElement>(null);
+  const { isOpen, setIsOpen } = useClickOutside(dropDownNode, false);
+  const name = localStorage.getItem('name');
   return (
     <div className={styles.userInfoContainer}>
       <div className={styles.userInfoAvatarContainer}>
         <img className={styles.userInfoAvatar} src="/svg/user.svg#user" alt="avatar" />
       </div>
-      <p className={styles.username}>{username}</p>
-      <div className={styles.arrow}></div>
+      <p className={styles.username}>{name}</p>
+      <div ref={dropDownNode} className={styles.arrow} onClick={() => setIsOpen((value) => !value)}>
+        {isOpen && (
+          <div
+            className={styles.userDropDownItem}
+            onClick={() => {
+              localStorage.setItem('isLogout', 'true');
+              window.location.reload();
+            }}
+          >
+            Выход
+          </div>
+        )}
+      </div>
     </div>
   );
 }
