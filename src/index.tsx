32edb27bbe20f4cd5c568c665e1ca552e5d26b02
bulation <insanbulat@gmail.com>
2023-02-10 21:18:26 +0300

    feat: add redux store and provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,15 @@ import App from './App';
 import './normalize.css';
 import { BrowserRouter } from 'react-router-dom';
 
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
