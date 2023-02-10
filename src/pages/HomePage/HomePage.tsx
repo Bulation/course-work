a321f08ab335c78759fb74fa9085f0a0b43d0104
bulation <insanbulat@gmail.com>
2023-02-10 23:23:20 +0300

    feat: add home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,39 @@
+import Header from '../../components/Header/Header';
+import styles from './homepage.module.css';
+import { presentationData } from '../../data/data';
+import Footer from '../../components/Footer/Footer';
+import HomeFilter from '../../components/HomeFilter/HomeFilter';
+import FlatsPresentation from '../../components/FlatsPresentation/FlatsPresentation';
+import FlatForDay from '../../components/FlatForDay/FlatForDay';
+import AsideNews from '../../components/AsideNews/AsideNews';
+import Proposals from '../../components/Proposals/Proposals';
+import FlatsSlider from '../../components/FlatsSlider/FlatsSlider';
+
 export default function HomePage() {
-  return <h1>a</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <HomeFilter />
+        <Proposals />
+        <FlatsSlider />
+        <section className={styles.searchFlatsSection}>
+          <div className={styles.searchFlatsContainer}>
+            <FlatsPresentation
+              title="Поиск квартир на карте"
+              description="Ищите квартиры на сутки в центре города, возле парка или в живописном районе"
+              presentationData={presentationData}
+            />
+          </div>
+        </section>
+        <section className={styles.flatForDaySection}>
+          <div className={styles.flatForDayContainer}>
+            <FlatForDay />
+            <AsideNews />
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 }
